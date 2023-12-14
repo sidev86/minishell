da8ebaa5ca7428d4ac8d88f20128a560ee64cb0b
sidev86 <samir.ibrahim@outlook.it>
2023-12-14 18:17:43 +0100

    removed warning problem with signal handler CTRL-C (valgr)

--- signals.c
+++ signals.c
@@ -16,9 +16,10 @@ void	signal_handler(int sig)
 {
 	if (sig == SIGINT)
 	{
-		ioctl(0, TIOCSTI, "\n");
+		write(1, "\n", 1);
 		rl_replace_line("", 0);
 		rl_on_new_line();
+		rl_redisplay();
 		errors_manager(SET_CODE, 130, NULL, NULL);
 	}
 }
